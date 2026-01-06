03dea08ebed008ac71b736df6e31561c76d24290
Manas Satish Singh <61553781+Manas300@users.noreply.github.com>
2026-01-06 03:03:30 -0500

    Time: 0 ms (100.00%) | Memory: 19.7 MB (11.67%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,13 +1,22 @@
-class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 0:
-            return [[]]
-        
-        perms = self.permute(nums[1:])
-        res = []
-        for p in perms:
-            for i in range(len(p) + 1):
-                p_copy = p.copy()
-                p_copy.insert(i, nums[0])
-                res.append(p_copy)
-        return res
+class Solution(object):
+    def permute(self, nums):
+        result = []
+
+        def backtrack(path):
+
+            # Base Case
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+
+            for num in nums:
+                # Constraint
+                if num in path:
+                    continue
+
+                path.append(num)
+                backtrack(path)
+                path.pop()
+
+        backtrack([])
+        return result
