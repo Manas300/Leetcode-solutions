765bc12dac1f612bbd1215d9399649dd90c6d69c
Manas Satish Singh <61553781+Manas300@users.noreply.github.com>
2026-02-13 11:55:20 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (45.06%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,8 +1,22 @@
+from typing import List
+
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
-        
-        for num in nums:
-            res += [subset + [num] for subset in res]
-        
-        return res
+        result = []
+
+        def backtrack(index, path):
+            # Base case: we considered all elements
+            if index == len(nums):
+                result.append(path[:])  # make a copy
+                return
+
+            # Decision 1: include nums[index]
+            path.append(nums[index])
+            backtrack(index + 1, path)
+            path.pop()  # undo choice (backtrack)
+
+            # Decision 2: skip nums[index]
+            backtrack(index + 1, path)
+
+        backtrack(0, [])
+        return result
