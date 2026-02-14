abe1b5983f41aba5cabec956b093ec680a270880
Manas Satish Singh <61553781+Manas300@users.noreply.github.com>
2026-02-14 12:37:43 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (26.75%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,21 +1,27 @@
+
+
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        stack = []
-        res = []
+        result = []   # stores all valid combinations
+        path = []     # current building string as a list of characters
 
-        def backtrack(openN, closedN):
-            if openN == closedN == n:
-                res.append("".join(stack))
+        def backtrack(open_count, close_count):
+            # If we have used all 2*n parentheses, save the result
+            if len(path) == 2 * n:
+                result.append("".join(path))  # convert list to string
                 return
 
-            if openN < n:
-                stack.append("(")
-                backtrack(openN + 1, closedN)
-                stack.pop()
-            if closedN < openN:
-                stack.append(")")
-                backtrack(openN, closedN + 1)
-                stack.pop()
+            # Choice 1: Add "(" if we still have opens left
+            if open_count < n:
+                path.append("(")                         # choose "("
+                backtrack(open_count + 1, close_count)   # explore
+                path.pop()                               # undo (backtrack)
 
-        backtrack(0, 0)
-        return res
+            # Choice 2: Add ")" only if it won't break validity
+            if close_count < open_count:
+                path.append(")")                         # choose ")"
+                backtrack(open_count, close_count + 1)   # explore
+                path.pop()                               # undo (backtrack)
+
+        backtrack(0, 0)   # start with 0 open and 0 close used
+        return result
